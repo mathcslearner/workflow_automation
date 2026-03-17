5c6f674aea5db3f67904a832ee451f5c3c34388d
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2026-03-17 15:09:59 -0400

    feat: error tracking setup

---
+++ sentry.server.config.ts
@@ -0,0 +1,28 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c6598b7a1d7ffab5792f8879cad488f4@o4511061315354624.ingest.us.sentry.io/4511061325512704",
+
+  integrations: [
+    // Add the Vercel AI SDK integration to sentry.server.config.ts
+    Sentry.vercelAIIntegration({
+      recordInputs: true,
+      recordOutputs: true,
+    }),
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"]}),
+  ],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  sendDefaultPii: true,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/db";
 import { inngest } from "./client";
+import * as Sentry from "@sentry/nextjs"
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { createOpenAI } from "@ai-sdk/openai"
 import { createAnthropic } from "@ai-sdk/anthropic"
@@ -15,23 +16,37 @@ export const execute = inngest.createFunction(
   async ({ event, step }) => {
     await step.sleep("pretend", "5s");
 
+    Sentry.logger.info("User triggered test log", {log_source: 'sentry_test'});
+    console.warn("Something is missing");
+    console.error("Something went wrong");
+
     const { steps: geminiSteps } = await step.ai.wrap(
       "gemini-generate-text",
       generateText, 
       {
-      model: google("gemini-2.5-flash"),
-      system: "You are a helpful assistant.",
-      prompt: "What is 2 + 2?",
-      }
+        model: google("gemini-2.5-flash"),
+        system: "You are a helpful assistant.",
+        prompt: "What is 2 + 2?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
+      },
     );
 
     const { steps: openaiSteps } = await step.ai.wrap(
       "openai-generate-text",
       generateText, 
       {
-      model: openai("gpt-4o"),
-      system: "You are a helpful assistant.",
-      prompt: "What is 2 + 2?",
+        model: openai("gpt-4o"),
+        system: "You are a helpful assistant.",
+        prompt: "What is 2 + 2?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       }
     );
 
@@ -39,9 +54,14 @@ export const execute = inngest.createFunction(
       "anthropic-generate-text",
       generateText, 
       {
-      model: anthropic("claude-sonnet-4-5"),
-      system: "You are a helpful assistant.",
-      prompt: "What is 2 + 2?",
+        model: anthropic("claude-sonnet-4-5"),
+        system: "You are a helpful assistant.",
+        prompt: "What is 2 + 2?",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
       }
     );
 

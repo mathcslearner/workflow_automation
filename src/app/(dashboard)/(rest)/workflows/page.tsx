90356d4ba5d93fc2d2b281f9992289b9e3e449f3
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2026-03-17 17:38:50 -0400

    feat: workflow crud operations

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,10 +1,23 @@
+import { WorkflowsList } from "@/features/workflows/components/workflows";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary"
 
 const Page = async () => {
     await requireAuth();
-    
+
+    prefetchWorkflows();
+
     return (
-        <p>Workflows</p>
+        <HydrateClient>
+            <ErrorBoundary fallback={<p>Error!</p>}>
+                <Suspense fallback={<p>Loading...</p>}>
+                    <WorkflowsList />
+                </Suspense>
+            </ErrorBoundary>
+        </HydrateClient>
     );
 };
 

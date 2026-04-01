8842d9620fa1c6650662b0a86cbab5ea3295dae2
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2026-04-01 16:09:33 -0400

    feat: workflows creation frontend

--- src/features/workflows/components/workflows.tsx
+++ src/features/workflows/components/workflows.tsx
@@ -1,13 +1,50 @@
 "use client"
 
-import { useSuspenseWorkflows } from "../hooks/use-workflows"
+import { useRouter } from "next/navigation";
+import { useCreateWorkflow, useSuspenseWorkflows } from "../hooks/use-workflows"
+import { EntityContainer, EntityHeader } from "@/components/entity-components";
+import { useUpgradeModal } from "@/hooks/use-upgrade-modal";
 
 export const WorkflowsList = () => {
     const workflows = useSuspenseWorkflows();
 
     return (
-        <p>
-            {JSON.stringify(workflows.data, null, 2)}
-        </p>
+        <div className="flex-1 flex justify-center items-center">
+            <p>
+                {JSON.stringify(workflows.data, null, 2)}
+            </p>
+        </div>
+    )
+}
+
+export const WorkflowsHeader = ({disabled}: {disabled?: boolean}) => {
+    const router = useRouter();
+    const createWorkflow = useCreateWorkflow();
+    const {handleError, modal} = useUpgradeModal();
+
+    const handleCreate = () => {
+        createWorkflow.mutate(undefined,  {
+            onSuccess: (data) => {
+                router.push(`/workflows/${data.id}`);
+            },
+            onError: (error) => {
+                handleError(error);
+            }
+        });
+    }
+
+    return (
+        <>
+            {modal}
+            <EntityHeader title="Workflows" description="Create and manage your workflows" onNew={handleCreate} newButtonLabel="New workflow" disabled={disabled} isCreating={createWorkflow.isPending} />
+        </>
+    )
+}
+
+export const WorkflowsContainer = ({children}: {children: React.ReactNode}) => {
+    return (
+        <EntityContainer header={<WorkflowsHeader />} search={<></>} pagination={<></>}>
+            {children}
+        </EntityContainer>
     )
 }

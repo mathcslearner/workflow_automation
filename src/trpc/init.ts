cab1050919ea27a01442d7cc0738b6b403fa36fe
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2026-04-01 20:02:37 -0400

    feat: workflows ui

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -3,12 +3,13 @@ import { polarClient } from '@/lib/polar';
 import { TRPCError, initTRPC } from '@trpc/server';
 import { headers } from 'next/headers';
 import { cache } from 'react';
+import superjson from "superjson"
 
 export const createTRPCContext = cache(async () => {
     return { userId: 'user_123' };
 });
 
-const t = initTRPC.create({});
+const t = initTRPC.create({transformer: superjson});
 
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;

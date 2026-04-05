3de3653bc6faa1cf6e737c80eb3211fba8ddfaa9
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2026-04-05 15:34:17 -0400

    feat: node execution background job + hooks

---
+++ src/features/executions/lib/executor-registry.ts
@@ -0,0 +1,17 @@
+import { NodeType } from "@/generated/prisma/client";
+import { NodeExecutor } from "../types";
+
+export const executorRegistry: Record<NodeType, NodeExecutor> = {
+    [NodeType.INITIAL]: manualTriggerExecutor,
+    [NodeType.MANUAL_TRIGGER]: manualTriggerExecutor,
+    [NodeType.HTTP_REQUEST]: httpRequestExecutor
+};
+
+export const getExecutor = (type: NodeType): NodeExecutor => {
+    const executor = executorRegistry[type];
+    if (!executor) {
+        throw new Error(`No executor found for node type: ${type}`);
+    }
+
+    return executor;
+}

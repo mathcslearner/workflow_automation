12bb6b4a86106c381f80ca1d79b2c06f69b17a36
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2026-04-04 22:11:46 -0400

    feat: manual workflow dialog form

--- src/features/triggers/components/base-trigger-node.tsx
+++ src/features/triggers/components/base-trigger-node.tsx
@@ -3,39 +3,51 @@
 import { BaseHandle } from "@/components/react-flow/base-handle";
 import { BaseNode, BaseNodeContent } from "@/components/react-flow/base-node";
 import { WorkflowNode } from "@/components/workflow-node";
-import { NodeProps, Position } from "@xyflow/react";
+import { NodeProps, Position, useReactFlow } from "@xyflow/react";
 import type { LucideIcon } from "lucide-react";
 import { ReactNode, memo } from "react";
 import Image from "next/image";
+import { NodeStatus, NodeStatusIndicator } from "@/components/react-flow/node-status-indicator";
 
 interface BaseTriggerNodeProps extends NodeProps {
     icon: LucideIcon | string;
     name: string;
     description?: string;
     children?: ReactNode;
-    // status?: nodeStatus;
+    status?: NodeStatus;
     onSettings?: () => void;
     onDoubleClick?: () => void;
 }
 
-export const BaseTriggerNode = memo(({id, icon: Icon, name, description, children, onSettings, onDoubleClick}: BaseTriggerNodeProps) => {
-    // TODO: add delete method
-    const handleDelete = () => {};
+export const BaseTriggerNode = memo(({id, icon: Icon, name, description, children, status="initial", onSettings, onDoubleClick}: BaseTriggerNodeProps) => {
+    const { setNodes, setEdges } = useReactFlow();
+    const handleDelete = () => {
+        setNodes((currentNodes) => {
+            const updatedNodes = currentNodes.filter((node) => node.id !== id);
+            return updatedNodes;
+        });
+
+        setEdges((currentEdges) => {
+            const updatedEdges = currentEdges.filter((edge) => edge.source !== id && edge.target !== id);
+            return updatedEdges;
+        });
+    };
 
     return (
         <WorkflowNode name={name} description={description} onDelete={handleDelete} onSettings={onSettings}>
-            {/* TODO: Wrap within NodeStatusIndicator */}
-            <BaseNode onDoubleClick={onDoubleClick} className="rounded-l-2xl relative group">
-                <BaseNodeContent>
-                    {typeof Icon === "string" ? (
-                        <Image src={Icon} alt={name} width={16} height={16} />
-                    ) : (
-                        <Icon className="size-4 text-muted-foreground" />
-                    )}
-                    {children}
-                    <BaseHandle id="source-1" type="source" position={Position.Right} />
-                </BaseNodeContent>
-            </BaseNode>
+            <NodeStatusIndicator status={status} variant="border" className="rounded-l-2xl">
+                <BaseNode status={status} onDoubleClick={onDoubleClick} className="rounded-l-2xl relative group">
+                    <BaseNodeContent>
+                        {typeof Icon === "string" ? (
+                            <Image src={Icon} alt={name} width={16} height={16} />
+                        ) : (
+                            <Icon className="size-4 text-muted-foreground" />
+                        )}
+                        {children}
+                        <BaseHandle id="source-1" type="source" position={Position.Right} />
+                    </BaseNodeContent>
+                </BaseNode>
+            </NodeStatusIndicator>
         </WorkflowNode>
     )
 })

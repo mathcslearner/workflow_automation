6ead386fbcb0d005eba64023ed007dbea1a1e104
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2026-04-11 12:18:54 -0400

    feat: anthropic node and executor

--- src/config/node-components.ts
+++ src/config/node-components.ts
@@ -1,4 +1,5 @@
 import { InitialNode } from "@/components/initial-node";
+import { AnthropicNode } from "@/features/executions/components/anthropic/node";
 import { HttpRequestNode } from "@/features/executions/components/http-request/node";
 import { GoogleFormTrigger } from "@/features/triggers/components/google-form-trigger/node";
 import { ManualTriggerNode } from "@/features/triggers/components/manual-trigger/node";
@@ -11,7 +12,8 @@ export const nodeComponents = {
     [NodeType.HTTP_REQUEST]: HttpRequestNode,
     [NodeType.MANUAL_TRIGGER]: ManualTriggerNode,
     [NodeType.GOOGLE_FORM_TRIGGER]: GoogleFormTrigger,
-    [NodeType.STRIPE_TRIGGER]: StripeTriggerNode
+    [NodeType.STRIPE_TRIGGER]: StripeTriggerNode,
+    [NodeType.ANTHROPIC]: AnthropicNode
 } as const satisfies NodeTypes;
 
 export type RegisteredNodeType = keyof typeof nodeComponents;
--- src/features/executions/lib/executor-registry.ts
+++ src/features/executions/lib/executor-registry.ts
@@ -4,13 +4,15 @@ import { httpRequestExecutor } from "../components/http-request/executor";
 import { manualTriggerExecutor } from "@/features/triggers/components/manual-trigger/executor";
 import { googleFormTriggerExecutor } from "@/features/triggers/components/google-form-trigger/executor";
 import { stripeTriggerExecutor } from "@/features/triggers/components/stripe-trigger/executor";
+import { anthropicExecutor } from "../components/anthropic/executor";
 
 export const executorRegistry: Record<NodeType, NodeExecutor<any>> = {
     [NodeType.INITIAL]: manualTriggerExecutor,
     [NodeType.MANUAL_TRIGGER]: manualTriggerExecutor,
     [NodeType.HTTP_REQUEST]: httpRequestExecutor,
     [NodeType.GOOGLE_FORM_TRIGGER]: googleFormTriggerExecutor,
-    [NodeType.STRIPE_TRIGGER]: stripeTriggerExecutor
+    [NodeType.STRIPE_TRIGGER]: stripeTriggerExecutor,
+    [NodeType.ANTHROPIC]: anthropicExecutor
 };
 
 export const getExecutor = (type: NodeType): NodeExecutor => {

9ba63dfe9fd27433fa51073fce1de450627b0c3e
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2026-05-12 22:53:48 -0400

    feat: update single execution page and create execution component

--- src/app/(dashboard)/(rest)/executions/[executionId]/page.tsx
+++ src/app/(dashboard)/(rest)/executions/[executionId]/page.tsx
@@ -1,4 +1,9 @@
+import { ExecutionView } from "@/features/executions/components/execution";
+import { prefetchExecution } from "@/features/executions/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface PageProps {
     params: Promise<{
@@ -10,8 +15,20 @@ const Page = async ({ params }: PageProps) => {
     await requireAuth();
     
     const { executionId } = await params;
+    prefetchExecution(executionId);
+
     return (
-        <p>Execution id: {executionId}</p>
+        <div className="p-4 md:px-10 md:py-6 h-full">
+            <div className="mx-auto max-w-screen-md w-full flex flex-col gap-y-8 h-full">
+                <HydrateClient>
+                    <ErrorBoundary fallback={<ExecutionsError />}>
+                        <Suspense fallback={<ExecutionsLoading />}>
+                            <ExecutionView executionId={executionId} />
+                        </Suspense>
+                    </ErrorBoundary>
+                </HydrateClient>
+            </div>
+        </div>
     );
 };
 
